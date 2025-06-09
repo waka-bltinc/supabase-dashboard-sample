0ced81b4ec8291a1708afd78733cff2725f960fd
Sho Wakatsuki <166351606+waka-bltinc@users.noreply.github.com>
2025-06-09 16:08:46 +0900

    feat: /へのアクセスは/auth/loginへリダイレクト

--- lib/supabase/middleware.ts
+++ lib/supabase/middleware.ts
@@ -46,7 +46,6 @@ export async function updateSession(request: NextRequest) {
 	} = await supabase.auth.getUser();
 
 	if (
-		request.nextUrl.pathname !== "/" &&
 		!user &&
 		!request.nextUrl.pathname.startsWith("/login") &&
 		!request.nextUrl.pathname.startsWith("/auth")
