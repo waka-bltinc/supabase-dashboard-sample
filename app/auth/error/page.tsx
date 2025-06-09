90aa6704b5d2dc20ddeea8f366440734eebb1d44
Sho Wakatsuki <166351606+waka-bltinc@users.noreply.github.com>
2025-06-09 15:54:29 +0900

    chore: フォーマット実行

--- app/auth/error/page.tsx
+++ app/auth/error/page.tsx
@@ -1,36 +1,36 @@
 import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
 
 export default async function Page({
-  searchParams,
+	searchParams,
 }: {
-  searchParams: Promise<{ error: string }>;
+	searchParams: Promise<{ error: string }>;
 }) {
-  const params = await searchParams;
+	const params = await searchParams;
 
-  return (
-    <div className="flex min-h-svh w-full items-center justify-center p-6 md:p-10">
-      <div className="w-full max-w-sm">
-        <div className="flex flex-col gap-6">
-          <Card>
-            <CardHeader>
-              <CardTitle className="text-2xl">
-                Sorry, something went wrong.
-              </CardTitle>
-            </CardHeader>
-            <CardContent>
-              {params?.error ? (
-                <p className="text-sm text-muted-foreground">
-                  Code error: {params.error}
-                </p>
-              ) : (
-                <p className="text-sm text-muted-foreground">
-                  An unspecified error occurred.
-                </p>
-              )}
-            </CardContent>
-          </Card>
-        </div>
-      </div>
-    </div>
-  );
+	return (
+		<div className="flex min-h-svh w-full items-center justify-center p-6 md:p-10">
+			<div className="w-full max-w-sm">
+				<div className="flex flex-col gap-6">
+					<Card>
+						<CardHeader>
+							<CardTitle className="text-2xl">
+								Sorry, something went wrong.
+							</CardTitle>
+						</CardHeader>
+						<CardContent>
+							{params?.error ? (
+								<p className="text-sm text-muted-foreground">
+									Code error: {params.error}
+								</p>
+							) : (
+								<p className="text-sm text-muted-foreground">
+									An unspecified error occurred.
+								</p>
+							)}
+						</CardContent>
+					</Card>
+				</div>
+			</div>
+		</div>
+	);
 }
--- components/tutorial/fetch-data-steps.tsx
+++ components/tutorial/fetch-data-steps.tsx
@@ -1,5 +1,5 @@
-import { TutorialStep } from "./tutorial-step";
 import { CodeBlock } from "./code-block";
+import { TutorialStep } from "./tutorial-step";
 
 const create = `create table notes (
   id bigserial primary key,
@@ -45,71 +45,71 @@ export default function Page() {
 `.trim();
 
 export function FetchDataSteps() {
-  return (
-    <ol className="flex flex-col gap-6">
-      <TutorialStep title="Create some tables and insert some data">
-        <p>
-          Head over to the{" "}
-          <a
-            href="https://supabase.com/dashboard/project/_/editor"
-            className="font-bold hover:underline text-foreground/80"
-            target="_blank"
-            rel="noreferrer"
-          >
-            Table Editor
-          </a>{" "}
-          for your Supabase project to create a table and insert some example
-          data. If you&apos;re stuck for creativity, you can copy and paste the
-          following into the{" "}
-          <a
-            href="https://supabase.com/dashboard/project/_/sql/new"
-            className="font-bold hover:underline text-foreground/80"
-            target="_blank"
-            rel="noreferrer"
-          >
-            SQL Editor
-          </a>{" "}
-          and click RUN!
-        </p>
-        <CodeBlock code={create} />
-      </TutorialStep>
+	return (
+		<ol className="flex flex-col gap-6">
+			<TutorialStep title="Create some tables and insert some data">
+				<p>
+					Head over to the{" "}
+					<a
+						href="https://supabase.com/dashboard/project/_/editor"
+						className="font-bold hover:underline text-foreground/80"
+						target="_blank"
+						rel="noreferrer"
+					>
+						Table Editor
+					</a>{" "}
+					for your Supabase project to create a table and insert some example
+					data. If you&apos;re stuck for creativity, you can copy and paste the
+					following into the{" "}
+					<a
+						href="https://supabase.com/dashboard/project/_/sql/new"
+						className="font-bold hover:underline text-foreground/80"
+						target="_blank"
+						rel="noreferrer"
+					>
+						SQL Editor
+					</a>{" "}
+					and click RUN!
+				</p>
+				<CodeBlock code={create} />
+			</TutorialStep>
 
-      <TutorialStep title="Query Supabase data from Next.js">
-        <p>
-          To create a Supabase client and query data from an Async Server
-          Component, create a new page.tsx file at{" "}
-          <span className="relative rounded bg-muted px-[0.3rem] py-[0.2rem] font-mono text-xs font-medium text-secondary-foreground border">
-            /app/notes/page.tsx
-          </span>{" "}
-          and add the following.
-        </p>
-        <CodeBlock code={server} />
-        <p>Alternatively, you can use a Client Component.</p>
-        <CodeBlock code={client} />
-      </TutorialStep>
+			<TutorialStep title="Query Supabase data from Next.js">
+				<p>
+					To create a Supabase client and query data from an Async Server
+					Component, create a new page.tsx file at{" "}
+					<span className="relative rounded bg-muted px-[0.3rem] py-[0.2rem] font-mono text-xs font-medium text-secondary-foreground border">
+						/app/notes/page.tsx
+					</span>{" "}
+					and add the following.
+				</p>
+				<CodeBlock code={server} />
+				<p>Alternatively, you can use a Client Component.</p>
+				<CodeBlock code={client} />
+			</TutorialStep>
 
-      <TutorialStep title="Explore the Supabase UI Library">
-        <p>
-          Head over to the{" "}
-          <a
-            href="https://supabase.com/ui"
-            className="font-bold hover:underline text-foreground/80"
-          >
-            Supabase UI library
-          </a>{" "}
-          and try installing some blocks. For example, you can install a
-          Realtime Chat block by running:
-        </p>
-        <CodeBlock
-          code={
-            "npx shadcn@latest add https://supabase.com/ui/r/realtime-chat-nextjs.json"
-          }
-        />
-      </TutorialStep>
+			<TutorialStep title="Explore the Supabase UI Library">
+				<p>
+					Head over to the{" "}
+					<a
+						href="https://supabase.com/ui"
+						className="font-bold hover:underline text-foreground/80"
+					>
+						Supabase UI library
+					</a>{" "}
+					and try installing some blocks. For example, you can install a
+					Realtime Chat block by running:
+				</p>
+				<CodeBlock
+					code={
+						"npx shadcn@latest add https://supabase.com/ui/r/realtime-chat-nextjs.json"
+					}
+				/>
+			</TutorialStep>
 
-      <TutorialStep title="Build in a weekend and scale to millions!">
-        <p>You&apos;re ready to launch your product to the world! 🚀</p>
-      </TutorialStep>
-    </ol>
-  );
+			<TutorialStep title="Build in a weekend and scale to millions!">
+				<p>You&apos;re ready to launch your product to the world! 🚀</p>
+			</TutorialStep>
+		</ol>
+	);
 }
